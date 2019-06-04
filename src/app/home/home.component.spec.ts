77e508f5f80795c8cce462dd1602b3048b5f7620
Dieter <dieter.jordens@continuum.be>
2019-06-04 08:36:13 +0200

    DJ-1 remove duplicate route test from home component.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -9,8 +9,6 @@ import {MockComponent} from 'ng-mocks';
 import {VisionComponent} from '../vision/vision.component';
 
 describe('HomeComponent', () => {
-  const homePath = '';
-  const visionPath = 'vision';
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
   let location: Location;
@@ -21,20 +19,14 @@ describe('HomeComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule.withRoutes([
-          {
-            path: homePath,
-            component: HomeComponent
-          },
-          {
-            path: visionPath,
-            component: MockComponent(VisionComponent)
-          }
+          {path: '', component: HomeComponent},
+          {path: 'vision', component: MockComponent(VisionComponent)}
         ])
       ],
       declarations: [
+        MockComponent(VisionComponent),
         HomeComponent,
-        TranslatePipeMock,
-        MockComponent(VisionComponent)
+        TranslatePipeMock
       ]
     })
       .compileComponents();
@@ -51,65 +43,41 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  describe('component routing', () => {
-    it('navigate to "" redirects you to the home path', fakeAsync(() => {
-      const expectedHomePath = '/' + homePath;
-      router.navigate(['']);
-      tick();
-      const actualCurrentPath = getCurrentPath();
-
-      expect(actualCurrentPath).toBe(expectedHomePath);
-    }));
-
-
-    it('clicking on the "learn more" button redirects you to the vision path', fakeAsync(() => {
-      const expectedVisionPath = '/' + visionPath;
-      clickLearnMoreButton();
-      tick();
-      const actualCurrentPath = getCurrentPath();
-
-      expect(actualCurrentPath).toBe(expectedVisionPath);
-    }));
-
-
-    function getCurrentPath(): string {
-      return location.path();
-    }
-
-    function clickLearnMoreButton(): void {
-      const visionButton = getLearnMoreButton();
-      visionButton.click();
-      fixture.detectChanges();
-    }
-  });
-
   it('should show the translated welcome message', () => {
     const expectedTranslatedWelcomeMessageText = 'website.welcome.message.translated';
-    const actualWelcomeMessageText = trimTextContent(getWelcomeMessage());
 
+    const actualWelcomeMessageText = trimTextContent(getWelcomeMessage());
     expect(actualWelcomeMessageText).toBe(expectedTranslatedWelcomeMessageText);
   });
 
   it('should show the translated introduction message', () => {
     const expectedTranslatedIntroductionMessageText = 'website.introduction.message.translated';
-    const actualIntroductionMessageText = trimTextContent(getIntroductionMessage());
 
+    const actualIntroductionMessageText = trimTextContent(getIntroductionMessage());
     expect(actualIntroductionMessageText).toBe(expectedTranslatedIntroductionMessageText);
   });
 
   it('should show the translated details message', () => {
     const expectedTranslatedDetailsMessageText = 'website.details.message.translated';
-    const actualDetailsMessageText = trimTextContent(getDetailsMessage());
 
+    const actualDetailsMessageText = trimTextContent(getDetailsMessage());
     expect(actualDetailsMessageText).toBe(expectedTranslatedDetailsMessageText);
   });
 
   it('should show the translated learn more button', () => {
     const expectedTranslatedLearnMoreText = 'website.learn.more.about.my.vision.translated';
-    const actualLearnMoreButtonText = trimTextContent(getLearnMoreButton());
 
+    const actualLearnMoreButtonText = trimTextContent(getLearnMoreButton());
     expect(actualLearnMoreButtonText).toBe(expectedTranslatedLearnMoreText);
   });
+
+  it('clicking on the "learn more" button redirects you to the vision path', fakeAsync(() => {
+    const expectedVisionPath = '/vision';
+    clickLearnMoreButton();
+
+    const actualCurrentPath = getCurrentPath();
+    expect(actualCurrentPath).toBe(expectedVisionPath);
+  }));
 
   function getLearnMoreButton(): HTMLButtonElement {
     return element.querySelector('.learnMore') as HTMLButtonElement;
@@ -129,5 +97,16 @@ describe('HomeComponent', () => {
 
   function trimTextContent(htmlElement: HTMLElement): string {
     return htmlElement.textContent.trim();
+  }
+
+  function getCurrentPath(): string {
+    return location.path();
+  }
+
+  function clickLearnMoreButton(): void {
+    const visionButton = getLearnMoreButton();
+    visionButton.click();
+    fixture.detectChanges();
+    tick();
   }
 });
