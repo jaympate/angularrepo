004ddc5c4341ea0f49cd8a269689344ad76dd12f
Dieter <dieter.jordens@continuum.be>
2019-06-17 00:09:02 +0200

    DJ-2 make it look a bit more stylish.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,17 +4,22 @@ import * as moment from 'moment';
 @Component({
   selector: 'app-home',
   template: `
-    <div class="jumbotron">
-      <h1 class="display-4 welcomeMessage">{{'website.welcome.message' | translate}}</h1>
-      <p class="lead introductionMessage">{{'website.introduction.message' | translate}}</p>
-      <hr class="my-4">
-      <p class="detailsMessage">{{'website.details.message' | translate:param}}</p>
-      <p class="lead">
-        <button class="btn btn-primary btn-lg learnMore" routerLink="vision" type="button"
-                role="button">{{'website.learn.more.about.my.vision' | translate}}</button>
-      </p>
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="home">
+          <p class="website">Dieter Jordens</p>
+          <h1 class="display-4 welcomeMessage">{{'website.welcome.message' | translate | uppercase}}</h1>
+          <p class="lead introductionMessage">{{'website.introduction.message' | translate}}</p>
+          <p class="detailsMessage">{{'website.details.message' | translate:param}}</p>
+          <p class="lead">
+            <button class="btn btn-lg learnMore" routerLink="vision" type="button"
+                    role="button">{{'website.learn.more.about.my.vision' | translate}}</button>
+          </p>
+        </div>
+      </div>
     </div>
-  `
+  `,
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   param = {age: HomeComponent.getAge()};
