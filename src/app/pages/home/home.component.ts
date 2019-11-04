f7914557c1521f8a641c4e136385912ccb341908
Dieter <dieter.jordens@continuum.be>
2019-11-04 14:07:00 +0100

    vertaling fod justitie en verwijdering knop.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,10 +11,6 @@ import * as moment from 'moment';
           <h1 class="welcomeMessage">{{'website.welcome.message' | translate | uppercase}}</h1>
           <p class="lead introductionMessage">{{'website.introduction.message' | translate}}</p>
           <p class="detailsMessage">{{'website.details.message' | translate:param}}</p>
-          <p class="pt-4">
-            <button class="btn btn-outline-dark btn-lg learnMore" routerLink="vision" type="button"
-                    role="button">{{'website.learn.more.about.my.vision' | translate}}</button>
-          </p>
         </div>
       </div>
     </div>
