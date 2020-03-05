9b06f98257dea3ecafef30f4a9f30a24a4a988b7
Dieter <dieter.jordens@continuum.be>
2020-03-05 22:44:17 +0100

    move blogposts to it's own page.
    also change the tests to use jest, still a lot to add though.

--- src/app/pages/blogpost/blogpost.component.ts
+++ src/app/pages/blogpost/blogpost.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-data',
   template: `
-    <div class="container-fluid pt-4 pb-4 ml-4">
+    <div class="container-fluid pt-4 pb-4">
       <h1>{{'blogpost.title' | translate}}</h1>
       <blogpost-overview></blogpost-overview>
     </div>
