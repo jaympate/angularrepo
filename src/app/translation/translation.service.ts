45ff922d80d693aa7dbbfa5c53ac87181119f851
Dieter <dieter.jordens@continuum.be>
2020-03-27 21:08:39 +0100

    update some versions.

--- src/app/translation/translation.service.ts
+++ src/app/translation/translation.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {combineLatest, Observable} from 'rxjs';
-import {map, tap} from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 import {BackendTranslationService} from './backend-translation.service';
 import {BackendTranslations} from './backend.translations';
 import {TranslationKeyValues} from './translation.key.values';
