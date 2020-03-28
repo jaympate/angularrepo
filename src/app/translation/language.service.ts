6778c3e350785a48ee0398cb5c45bb06794333a1
Dieter <dieter.jordens@continuum.be>
2020-03-28 18:51:55 +0100

    use localstorage instead of cookies.

--- src/app/translation/language.service.ts
+++ src/app/translation/language.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {CookieService} from 'ngx-cookie-service';
 import {Languages} from './languages';
 import {TranslateServiceFacade} from './translate.service.facade';
+import {LocalStorageService} from 'ngx-localstorage';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,9 @@ export class LanguageService {
   private readonly LANGUAGE_COOKIE = 'website.locale';
   private currentAndOtherLanguagesBehaviorSubject: BehaviorSubject<Languages>;
 
-  constructor(private translateService: TranslateServiceFacade, private cookieService: CookieService) {
-    const currentLanguage = cookieService.get(this.LANGUAGE_COOKIE) || this.translateService.getDefaultLanguage();
+  constructor(private translateService: TranslateServiceFacade, private localStorage: LocalStorageService) {
+    console.log(localStorage.get(this.LANGUAGE_COOKIE));
+    const currentLanguage = localStorage.get(this.LANGUAGE_COOKIE) || this.translateService.getDefaultLanguage();
     this.currentAndOtherLanguagesBehaviorSubject = new BehaviorSubject<Languages>(this.getLanguages(currentLanguage));
     this.translateService.use(currentLanguage);
   }
@@ -22,7 +23,7 @@ export class LanguageService {
   }
 
   updateCurrentLanguage(currentLanguage: string): void {
-    this.cookieService.set(this.LANGUAGE_COOKIE, currentLanguage);
+    this.localStorage.set(this.LANGUAGE_COOKIE, currentLanguage);
     this.translateService.use(currentLanguage);
     this.currentAndOtherLanguagesBehaviorSubject.next(this.getLanguages(currentLanguage));
   }
--- src/app/translation/translation.module.ts
+++ src/app/translation/translation.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {CustomTranslateLoaderFactory} from './custom.translate.loader.factory';
 import {TranslationService} from './translation.service';
-import {CookieService} from 'ngx-cookie-service';
+import {NgxLocalStorageModule} from 'ngx-localstorage';
 
 @NgModule({
   imports: [
+    NgxLocalStorageModule.forRoot(),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -15,9 +16,6 @@ import {CookieService} from 'ngx-cookie-service';
         ]
       }
     })
-  ],
-  providers: [
-    CookieService
   ],
   exports: [
     TranslateModule
