433282fcfee79dfbd7ed3495bc1a40fbc1036997
Dieter <dieter.jordens@continuum.be>
2020-06-17 23:34:26 +0200

    add google analytics first version to try out.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {filter} from 'rxjs/operators';
+
+
+// declare ga as a function to set and sent the events
+declare let ga: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private routerSubscription: Subscription;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    // subscribe to router events and send page views to Google Analytics
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
+  }
 }
