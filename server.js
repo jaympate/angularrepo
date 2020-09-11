a2ac7497fc5f579196902d7c23571e65cb3035b6
Dieter Jordens <dieter.jordens.website@gmail.com>
2020-09-11 17:49:10 +0200

    attempt to get https working.

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const compression = require('compression');
-const sslRedirect = require('heroku-ssl-redirect');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/website'));
 app.use(compression());
-app.use(sslRedirect());
 
 app.route('/sitemap.xml').get((req, res) => {
   res.sendFile(path.resolve(path.join(__dirname, '/sitemap.xml')));
