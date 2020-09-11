0e4ec223e9ce4efb211063f7ce2b8ba1a9e13176
Dieter Jordens <dieter.jordens.website@gmail.com>
2020-09-11 18:26:28 +0200

    get old version back for server.js

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const app = express();
 const compression = require('compression');
-const forceSsl =  (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
-};
 
-const options = {
-  key: fs.readFileSync(__dirname + '/ssl/example.key'),
-  cert: fs.readFileSync(__dirname + '/ssl/example.crt')
-};
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/website'));
 app.use(compression());
-app.use(forceSsl)
 
 app.route('/sitemap.xml').get((req, res) => {
   res.sendFile(path.resolve(path.join(__dirname, '/sitemap.xml')));
@@ -28,9 +16,5 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/website/index.html'));
 });
 
-const https = require('https')
-
-https.createServer(options, app)
-  .listen(process.env.PORT || 8080, () => {
-    console.log("server running!")
-  });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
