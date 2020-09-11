158e9fc82c3ade62502190df869830b4dbce012f
Dieter Jordens <dieter.jordens.website@gmail.com>
2020-09-11 16:19:26 +0200

    attempt to get https working.

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 //Install express server
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const app = express();
-const compression = require('compression');
 
+const credentials = {
+  key: fs.readFileSync(__dirname + '/ssl/example.key'),
+  cert: fs.readFileSync(__dirname + '/ssl/example.crt')
+}
+
+const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/website'));
-app.use(compression());
 
 app.route('/sitemap.xml').get((req, res) => {
   res.sendFile(path.resolve(path.join(__dirname, '/sitemap.xml')));
@@ -16,5 +22,8 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/website/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(443);
