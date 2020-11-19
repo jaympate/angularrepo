65631b5954d8ef0791b65d276d2828ff54d95e82
dieter.jordens <dieter.jordens@continuum.be>
2020-11-19 20:42:22 +0100

    Add some updates to the pictures

--- src/app/pages/vision/software-craftsmanship-card/software-craftsmanship-card.component.ts
+++ src/app/pages/vision/software-craftsmanship-card/software-craftsmanship-card.component.ts
@@ -4,19 +4,16 @@ import {Card} from './card';
 @Component({
   selector: 'app-software-craftsmanship-card',
   template: `
-    <ng-container *ngIf="!!card">
-      <img
-        class="card-img-top"
-        src="{{ card.src }}"
-        alt="{{ card.alt | translate }}"
+    <div class="text-center">
+      <img src="{{ card.src }}" height="200" alt="{{ card.alt | translate }}"
       />
-      <div class="card-body">
-        <h5 class="card-title">{{ card.title | translate }}</h5>
-        <ng-container *ngFor="let paragraph of card.paragraphs">
-          <p class="card-text">{{ paragraph | translate }}</p>
-        </ng-container>
-      </div>
-    </ng-container>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">{{ card.title | translate }}</h5>
+      <ng-container *ngFor="let paragraph of card.paragraphs">
+        <p class="card-text">{{ paragraph | translate }}</p>
+      </ng-container>
+    </div>
   `,
   styles: [
       `
