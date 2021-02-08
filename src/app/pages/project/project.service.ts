d69e394bb41f73bb7b7c732e3a239bed5be3e344
dieter.jordens <dieter.jordens@continuum.be>
2021-02-08 16:19:56 +0100

    update website to work with responsibilities instead of description

--- src/app/pages/project/project.service.ts
+++ src/app/pages/project/project.service.ts
@@ -1,70 +1,26 @@
 import { Injectable } from '@angular/core';
-import { combineLatest, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { TranslateServiceFacade } from '../../translation/translate.service.facade';
-import { map, tap } from 'rxjs/operators';
 import { Project } from './project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
-  private untranslatedProjects: Project[];
-  private readonly translatedProjects$: Observable<Project[]>;
+  private readonly baseUrl = `https://www.dieterjordens.com:10002/api/projects`;
+  private readonly projects$: Observable<Project[]>;
 
-  constructor(
-    private http: HttpClient,
-    private translateService: TranslateServiceFacade
-  ) {
-    const currentLanguage$ = translateService.getCurrentLanguage$();
+  constructor(private http: HttpClient) {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         Authorization: 'Basic YWRtaW46RWVuRWVudm91ZGlnV2FjaHR3b29yZA=='
       })
     };
-    const projects$: Observable<Project[]> = this.http.get<Project[]>(
-      this.baseUrl,
-      httpOptions
-    );
-
-    this.translatedProjects$ = combineLatest([
-      projects$,
-      currentLanguage$
-    ]).pipe(
-      map(([projects]) => projects),
-      tap((projects) => this.cacheProjects(projects)),
-      map(() => this.translateProjects())
-    );
+    this.projects$ = this.http.get<Project[]>(this.baseUrl, httpOptions);
   }
-
-  private readonly baseUrl = `https://www.dieterjordens.com:10002/api/projects`;
 
   getProjects$(): Observable<Project[]> {
-    return this.translatedProjects$;
-  }
-
-  private cacheProjects(projects: Project[]) {
-    if (!this.untranslatedProjects) {
-      this.untranslatedProjects = projects;
-    }
-  }
-
-  private translateProjects(): Project[] {
-    return this.untranslatedProjects.map((project) => ({
-      ...project,
-      client: this.translateService.getTranslationKnowingTheyAreLoaded(
-        project.client
-      ),
-      jobTitle: this.translateService.getTranslationKnowingTheyAreLoaded(
-        project.jobTitle
-      ),
-      jobDescription: this.translateService.getTranslationKnowingTheyAreLoaded(
-        project.jobDescription
-      ),
-      timeSpan: this.translateService.getTranslationKnowingTheyAreLoaded(
-        project.timeSpan
-      )
-    }));
+    return this.projects$;
   }
 }
