9277b4a49608a5f68e0ab3b2711b1aeec03d8f3b
dieter.jordens <dieter.jordens@continuum.be>
2021-02-15 14:10:22 +0100

    add some tests

---
+++ src/app/pages/article/article.service.spec.ts
@@ -0,0 +1,81 @@
+import {TestBed} from '@angular/core/testing';
+import {ArticleService} from './article.service';
+import {HttpClient} from '@angular/common/http';
+import {of, throwError} from 'rxjs';
+import {Article} from './article';
+import {Builder} from 'builder-pattern';
+
+
+describe('ArticleService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ArticleService,
+        HttpClient
+      ]
+    });
+  });
+
+  it('returns the articles observable of the backend', async () => {
+    const articlesBackend = [
+      Builder<Article>()
+        .url('http://www.google.be')
+        .title('an article of google')
+        .build(),
+      Builder<Article>()
+        .url('http://www.medium.com')
+        .title('an article of medium')
+        .build()
+    ];
+
+    const get = jest.fn().mockReturnValue(of(articlesBackend));
+    TestBed.overrideProvider(HttpClient, { useFactory: () => ({get})});
+
+    const articleService = TestBed.inject(ArticleService);
+
+    const actualArticles = await articleService.getArticles$().toPromise();
+
+    expect(actualArticles).toEqual(articlesBackend);
+  });
+
+  it('calls the backend with the right configuration', async () => {
+    const get = jest.fn().mockReturnValue(of([]));
+    TestBed.overrideProvider(HttpClient, { useFactory: () => ({get})});
+
+    const articleService = TestBed.inject(ArticleService);
+    const httpClient = TestBed.inject(HttpClient);
+
+    await articleService.getArticles$().toPromise();
+
+    expect(httpClient.get).toHaveBeenCalledWith(`https://www.dieterjordens.com/api/articles`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Basic YWRtaW46RWVuRWVudm91ZGlnV2FjaHR3b29yZA=='
+        }
+    });
+  });
+
+  it('logs an error when an error occurs', async () => {
+    const errorSpy = spyOn(console, 'error');
+    const message = 'An error';
+    const get = jest.fn().mockReturnValue(throwError({message}));
+    TestBed.overrideProvider(HttpClient, { useFactory: () => ({get})});
+
+    const articleService = TestBed.inject(ArticleService);
+
+    await articleService.getArticles$().toPromise();
+
+    expect(errorSpy).toHaveBeenCalledWith(`An error has occurred when retrieving articles: ${message}`);
+  });
+
+  it('returns an empty array of articles when an error occurs', async () => {
+    spyOn(console, 'error');
+    const get = jest.fn().mockReturnValue(throwError({}));
+    TestBed.overrideProvider(HttpClient, { useFactory: () => ({get})});
+    const articleService = TestBed.inject(ArticleService);
+
+    const actualArticles = await articleService.getArticles$().toPromise();
+
+    expect(actualArticles).toEqual([]);
+  });
+});
--- src/app/pages/article/article.service.ts
+++ src/app/pages/article/article.service.ts
@@ -1,23 +1,29 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Article} from './article';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
   private readonly baseUrl = `https://www.dieterjordens.com/api/articles`;
+  private readonly httpOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Basic YWRtaW46RWVuRWVudm91ZGlnV2FjaHR3b29yZA=='
+    }
+  };
 
-  constructor(private http: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
   getArticles$(): Observable<Article[]> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: 'Basic YWRtaW46RWVuRWVudm91ZGlnV2FjaHR3b29yZA=='
+    return this.httpClient.get<Article[]>(this.baseUrl, this.httpOptions).pipe(
+      catchError((error: Error) => {
+        console.error(`An error has occurred when retrieving articles: ${error.message}`);
+        return of([]);
       })
-    };
-    return this.http.get<Article[]>(this.baseUrl, httpOptions);
+    );
   }
 }
