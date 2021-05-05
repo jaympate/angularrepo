8c8c8b76264ecb84e84ecff3b85f3924dda546aa
dieter.jordens <dieter.jordens@continuum.be>
2021-05-05 10:49:32 +0200

    optimalization, only load the google analytics after the view has been initialized.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,12 +15,26 @@ export class AppComponent implements AfterViewInit, OnDestroy {
 
   constructor(private router: Router) {}
 
+  loadGoogleAnalytics(trackingID: string): void {
+    const gaScript = document.createElement('script');
+    gaScript.setAttribute('async', 'true');
+    gaScript.setAttribute('src', `https://www.googletagmanager.com/gtag/js?id=${ trackingID }`);
+
+    const gaScript2 = document.createElement('script');
+    gaScript2.innerText = `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag(\'js\', new Date());`;
+
+    document.documentElement.firstChild.appendChild(gaScript);
+    document.documentElement.firstChild.appendChild(gaScript2);
+  }
+
   ngAfterViewInit(): void {
+    const trackingId = 'UA-169820783-1';
+    this.loadGoogleAnalytics(trackingId);
     // subscribe to router events and send page views to Google Analytics
     this.routerSubscription = this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
-        gtag('config', 'UA-169820783-1', {
+        gtag('config', trackingId, {
           page_path: event.urlAfterRedirects
         });
       });
